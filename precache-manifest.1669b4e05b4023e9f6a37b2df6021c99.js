22e32f9cfe9b811e5633bd817607e7524a11e186
Mirsat Sefidanoski <mirsat1994@gmail.com>
2020-06-09 13:32:08 +0200

    Updates

---
+++ precache-manifest.1669b4e05b4023e9f6a37b2df6021c99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30eb3093287a2e365e0dbc5d6d67cce",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "c5d782508ca1db4f8705",
+    "url": "/my-app2/static/js/2.37c6755d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2/static/js/2.37c6755d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be58579afc5a110e24f",
+    "url": "/my-app2/static/js/main.6d5026a4.chunk.js"
+  },
+  {
+    "revision": "41f012a7da5aa159ec7d",
+    "url": "/my-app2/static/js/runtime-main.03f48c46.js"
+  }
+]);
